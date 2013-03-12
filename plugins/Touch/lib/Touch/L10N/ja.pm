3ae4523dfb162547997aecb9cdf7ec849dcc26b7
Alfasado Inc <webmaster@alfasado.jp>
2013-03-12 15:40:37 +0900

    first commit

---
+++ plugins/Touch/lib/Touch/L10N/ja.pm
@@ -0,0 +1,10 @@
+package Touch::L10N::ja;
+use strict;
+use base 'Touch::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Touch file at rebuild.' => 'File モディファイアで指定されたファイルを touch します。',
+ );
+
+1;
